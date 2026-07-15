0ba3212f2e79aad2585258313f5e338e8089f755
Bodo <47588175+Bodowin@users.noreply.github.com>
2026-07-15 12:31:26 +0200

    Wire global recovery and production guards

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { App } from './App'
 import UpdatePrompt from './components/UpdatePrompt'
+import { AppErrorBoundary } from './components/AppErrorBoundary'
+import { ProductionGuard } from './components/ProductionGuard'
+import { installRuntimeDiagnostics } from './lib/runtimeDiagnostics'
+
+installRuntimeDiagnostics()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-    <UpdatePrompt />
+    <AppErrorBoundary>
+      <App />
+      <ProductionGuard />
+      <UpdatePrompt />
+    </AppErrorBoundary>
   </StrictMode>,
 )
