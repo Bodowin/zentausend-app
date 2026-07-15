ce3af9b9f156bd06755594e945dd750ca7fe3679
Bodo <47588175+Bodowin@users.noreply.github.com>
2026-07-15 12:42:23 +0200

    Load Safari overflow hardening styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './production-hardening.css'
 import { App } from './App'
 import UpdatePrompt from './components/UpdatePrompt'
 import { AppErrorBoundary } from './components/AppErrorBoundary'
